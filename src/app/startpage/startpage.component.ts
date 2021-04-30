dc0e456533f1943c72be5e9afe2b9f2361dcb334
AlterHowdegen <johannes.nichell@googlemail.com>
2021-04-30 17:16:04 +0200

    1 binaries uploaded

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+declare var require: any;
 
 @Component({
-  selector: 'app-startpage',
-  templateUrl: './startpage.component.html',
-  styleUrls: ['./startpage.component.css']
+  selector: "app-startpage",
+  templateUrl: "./startpage.component.html",
+  styleUrls: ["./startpage.component.css"]
 })
 export class StartpageComponent implements OnInit {
+  public itchBadge = require("./assets/itchBadgeColor.png");
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
