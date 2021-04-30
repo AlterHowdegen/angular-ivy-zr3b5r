b684860413f17ab34daa17985cd5b917ebb3307b
AlterHowdegen <johannes.nichell@googlemail.com>
2021-04-30 16:10:07 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,31 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NgxTwitterTimelineModule } from "ngx-twitter-timeline";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
 import { LeaderboardService } from "./leaderboard.service";
 import { LeaderboardResolver } from "./leaderboard.resolver";
+import { StartpageComponent } from "./startpage/startpage.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    NgxTwitterTimelineModule,
     RouterModule.forRoot([
       {
         path: "",
+        component: StartpageComponent,
+        resolve: {
+          leaderboard: LeaderboardResolver
+        }
+      },
+      {
+        path: "leaderboard",
         component: LeaderboardComponent,
         resolve: {
           leaderboard: LeaderboardResolver
@@ -26,7 +36,12 @@ import { LeaderboardResolver } from "./leaderboard.resolver";
       //      { path: "products/:productId", component: ProductDetailsComponent }
     ])
   ],
-  declarations: [AppComponent, HelloComponent, LeaderboardComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    LeaderboardComponent,
+    StartpageComponent
+  ],
   bootstrap: [AppComponent],
   providers: [LeaderboardService]
 })
